ea7c23a8965188c3ff0121682f10ffa1d9f68fd0
nareshs2812 <naresh.s2023cse@sece.ac.in>
2025-07-15 15:40:13 +0530

    Unwanted

--- Springboot-ems/src/test/java/com/example/springbootfirst/controllers/EmployeeControllerTest.java
+++ Springboot-ems/src/test/java/com/example/springbootfirst/controllers/EmployeeControllerTest.java
@@ -1,48 +1,48 @@
-package com.example.springbootfirst.controllers;
-
-import com.example.springbootfirst.controllers.EmployeeController;
-import com.example.springbootfirst.models.RegisterDetails;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import com.example.springbootfirst.services.AuthService;
-
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-public class EmployeeControllerTest {
-
-    @Mock
-    //AuthService authService;
-
-    @InjectMocks
-    EmployeeController employeeController;
-
-    @BeforeEach
-    void setup() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void testroute() {
-        String result = employeeController.route();
-        assertEquals("springboot ", result);
-    }
-
-    @Test
-    void testgetMethod() {
-        RegisterDetails emp1 = new RegisterDetails();
-        RegisterDetails emp2 = new RegisterDetails();
-        when(authService.getMethod()).thenReturn(Arrays.asList(emp1, emp2));
-
-        List<RegisterDetails> result = employeeController.getMethod();
-
-        assertEquals(2, result.size());
-    }
-}
+//package com.example.springbootfirst.controllers;
+//
+//import com.example.springbootfirst.controllers.EmployeeController;
+//import com.example.springbootfirst.models.RegisterDetails;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import com.example.springbootfirst.services.AuthService;
+//
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.Mockito.when;
+//
+//public class EmployeeControllerTest {
+//
+//    @Mock
+//    //AuthService authService;
+//
+//    @InjectMocks
+//    EmployeeController employeeController;
+//
+//    @BeforeEach
+//    void setup() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    void testroute() {
+//        String result = employeeController.route();
+//        assertEquals("springboot ", result);
+//    }
+//
+//    @Test
+//    void testgetMethod() {
+//        RegisterDetails emp1 = new RegisterDetails();
+//        RegisterDetails emp2 = new RegisterDetails();
+//        when(authService.getMethod()).thenReturn(Arrays.asList(emp1, emp2));
+//
+//        List<RegisterDetails> result = employeeController.getMethod();
+//
+//        assertEquals(2, result.size());
+//    }
+//}
