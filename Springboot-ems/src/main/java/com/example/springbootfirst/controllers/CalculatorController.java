7cfb43eb8668ce706c3677f6351cf5cdfe1b693e
nareshs2812 <naresh.s2023cse@sece.ac.in>
2025-07-15 09:37:37 +0530

    Day 10 Task

--- Springboot-ems/src/main/java/com/example/springbootfirst/controllers/CalculatorController.java
+++ Springboot-ems/src/main/java/com/example/springbootfirst/controllers/CalculatorController.java
@@ -1,22 +1,27 @@
 package com.example.springbootfirst.controllers;
 
 public class CalculatorController {
-    public String CalculatorTest(){
-        public int addNumbers(int a, int b) {
-            return a+b;
-        }
-        public int subtractNumbers(int a, int b) {
-            return a-b;
-        }
-        public int multiplyNumbers(int a, int b) {
-            return a*b;
-        }
-        public int divideNumbers(int a, int b) {
-            return a/b;
-        }
 
-        public int moduloNumbers(int a, int b) {
-            return a%b;
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplyNumbers(int a, int b) {
+        return a * b;
+    }
+
+    public int divideNumbers(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
         }
+        return a / b;
+    }
+
+    public int moduloNumbers(int a, int b) {
+        return a % b;
     }
 }
